299e4b139abbb5150517c9ed2cb4c275aea74d6d
flickylabs <flickylabs@gmail.com>
2026-05-02 08:26:31 +0900

    fix(api): use nodenext import extensions

--- api/llm/dialogue.ts
+++ api/llm/dialogue.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from 'node:http'
-import { handleChatCompletion } from '../../serverless/openaiProxy'
+import { handleChatCompletion } from '../../serverless/openaiProxy.js'
 
 export default async function handler(req: IncomingMessage, res: ServerResponse): Promise<void> {
   await handleChatCompletion(req, res, {
